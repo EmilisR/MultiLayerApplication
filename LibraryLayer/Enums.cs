3249c5a6daa2a512be26f1649351331455034676
Emilis Ruzveltas <E.Ruzveltas@post.lt>
2017-10-19 13:57:27 +0300

    'a'

---
+++ LibraryLayer/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryLayer
+{
+    public class Enums
+    {
+        public enum PaymentType
+        {
+            Cash,
+            CreditCard
+        }
+        
+        public enum ProductCategory
+        {
+            TV,
+            Computers,
+            Media,
+            Games
+        }
+    }
+}
