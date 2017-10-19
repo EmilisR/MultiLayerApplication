5deb030026d4f63dd3a71cb95c69c92513e93fc1
Emilis Ruzveltas <emilis.ruzveltas@gmail.com>
2017-10-19 21:16:49 +0300

    entities

--- DatabaseLayer/Basket.cs
+++ DatabaseLayer/Basket.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +11,13 @@ namespace DatabaseLayer
 {
     public class Basket
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public PaymentType PaymentType { get; set; }
         public double TotalPrice { get; set; }
-        public BasketItem[] BasketItems { get; set; }
+        public virtual BasketItem[] BasketItems { get; set; }
+        public virtual Customer Customer { get; set; }
 
     }
 }
