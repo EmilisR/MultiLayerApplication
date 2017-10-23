cc653dee38edb22bcba02e867f40ce843d65ac76
Emilis Ruzveltas <E.Ruzveltas@post.lt>
2017-10-23 14:13:56 +0300

    fix

--- WcfServiceLibrary1/ILoginService.cs
+++ WcfServiceLibrary1/ILoginService.cs
@@ -5,23 +5,20 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-namespace WcfServiceLibrary1
+namespace LoginService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface ILoginService
     {
         [OperationContract]
-        string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
+        bool Login(string email, string password);
 
         // TODO: Add your service operations here
     }
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "WcfServiceLibrary1.ContractType".
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "LoginService.ContractType".
     [DataContract]
     public class CompositeType
     {
--- WcfServiceLibrary1/Properties/AssemblyInfo.cs
+++ WcfServiceLibrary1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WcfServiceLibrary1")]
+[assembly: AssemblyTitle("LoginService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("WcfServiceLibrary1")]
+[assembly: AssemblyProduct("LoginService")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
